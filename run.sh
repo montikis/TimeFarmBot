9f3e24f761c386f63b53d50ac60c3d9bd8cb2f72
Montix <stalker.ghost.zone@gmail.com>
2024-07-12 20:58:25 +0500

    Run up

--- run.sh
+++ run.sh
@@ -1,41 +1,49 @@
 #!/bin/bash
 
 #Проверка обновления репозитория
-git pull --allow-unrelated-histories
+git pull --allow-unrelated-histories #принудительное обновление
+
+#установка библиотек если такие появились
+pip install -r requirements.txt
+#python3 --version
+
+#dmesg | grep "Linux version"
+
+
 
 # Проверка на наличие папки venv
 if [ ! -d "venv" ]; then
-    echo "Creating virtual environment..."
+    echo "💻 Creating virtual environment..."
     python3 -m venv venv
 fi
 
-echo "Activating virtual environment..."
+echo "✅  Activating virtual environment..."
 source venv/bin/activate
 
 # Проверка на наличие установленного флага в виртуальном окружении
 if [ ! -f "venv/installed" ]; then
     if [ -f "requirements.txt" ]; then
-		echo "Installing wheel for faster installing"
+		echo "💬 Installing wheel for faster installing"
 		pip3 install wheel
         echo "Installing dependencies..."
         pip3 install -r requirements.txt
         touch venv/installed
     else
-        echo "requirements.txt not found, skipping dependency installation."
+        echo "📄 requirements.txt not found, skipping dependency installation."
     fi
 else
-    echo "Dependencies already installed, skipping installation."
+    echo "💬 Dependencies already installed, skipping installation."
 fi
 
 if [ ! -f ".env" ]; then
-	echo "Copying configuration file"
+	echo "💬 Copying configuration file"
 	cp .env-example .env
 else
-	echo "Skipping .env copying"
+	echo "💬 Skipping .env copying"
 fi
 
-echo "Starting the bot..."
+echo "🤖 Starting the bot..."
 python3 main.py
 
-echo "done"
-echo "PLEASE EDIT .ENV FILE"
+echo "✅ done"
+echo "⚙️ PLEASE EDIT .ENV FILE"
